ec8e4c983bc570ef4d942a411d5ab02d4f11e028
“ssjosyula” <sandeepsarma.josyula@gmail.com>
2023-12-23 15:11:22 -0500

    Added itemitem to the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -33,60 +33,104 @@ class _AppLandingPageState extends State<AppLandingPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
           title: const Text("VeryClear"),
+          centerTitle: false,
           actions: [
-            IconButton(
-              onPressed: () => {
-                showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (BuildContext context) {
-                    return Padding(
-                      padding: MediaQuery.of(context).viewInsets,
-                      child: Container(
-                        width: double.infinity,
-                        height: 400,
-                        child: Form(
-                          key: _formKey,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-                            child: Column(
-                              children: [
-                                const Text('Add an item to compare'),
-                                TextFormField(
-                                  validator: (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Description required';
-                                    }
-                                    return null;
-                                  },
-                                  decoration: const InputDecoration(
-                                    prefixIcon: Icon(
-                                      Icons.add_shopping_cart,
-                                      color: Colors.black,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton.icon(
+                label: const Text('Add Item'),
+                icon: const Icon(
+                  Icons.add_circle_outlined,
+                  color: Colors.black,
+                ),
+                onPressed: () => {
+                  showModalBottomSheet(
+                    context: context,
+                    isScrollControlled: true,
+                    builder: (BuildContext context) {
+                      return Padding(
+                        padding: MediaQuery.of(context).viewInsets,
+                        child: Container(
+                          width: double.infinity,
+                          height: screenHeight * 0.50,
+                          child: Form(
+                            key: _formKey,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  20.0, 8.0, 20.0, 8.0),
+                              child: Column(
+                                children: [
+                                  TextFormField(
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Description required';
+                                      }
+                                      return null;
+                                    },
+                                    decoration: const InputDecoration(
+                                      prefixIcon: Icon(
+                                        Icons.add_shopping_cart_outlined,
+                                        color: Colors.black,
+                                      ),
+                                      hintText:
+                                          'Example : "Moo" Brand Organic Milk',
+                                      hintStyle: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                      labelText: 'Item Description',
                                     ),
-                                    hintText:
-                                        'Example : "Moo" Brand Organic Milk',
-                                    hintStyle: TextStyle(
-                                      fontStyle: FontStyle.italic,
-                                      fontWeight: FontWeight.w300,
-                                    ),
-                                    labelText: 'Item Description',
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: TextFormField(
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return 'Cost required';
+                                              }
+                                              return null;
+                                            },
+                                            keyboardType: const TextInputType
+                                                .numberWithOptions(
+                                              decimal: true,
+                                              signed: false,
+                                            ),
+                                            decoration: const InputDecoration(
+                                              prefixIcon: Icon(
+                                                Icons.attach_money_outlined,
+                                                color: Colors.black,
+                                              ),
+                                              hintText: '',
+                                              hintStyle: TextStyle(
+                                                fontStyle: FontStyle.italic,
+                                                fontWeight: FontWeight.w300,
+                                              ),
+                                              labelText: 'Cost',
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
                                     children: [
                                       Expanded(
                                         child: TextFormField(
                                           validator: (value) {
                                             if (value == null ||
                                                 value.isEmpty) {
-                                              return 'Cost required';
+                                              return 'Quantity required';
                                             }
                                             return null;
                                           },
@@ -97,7 +141,7 @@ class _AppLandingPageState extends State<AppLandingPage> {
                                           ),
                                           decoration: const InputDecoration(
                                             prefixIcon: Icon(
-                                              Icons.attach_money,
+                                              Icons.scale_outlined,
                                               color: Colors.black,
                                             ),
                                             hintText: '',
@@ -105,75 +149,64 @@ class _AppLandingPageState extends State<AppLandingPage> {
                                               fontStyle: FontStyle.italic,
                                               fontWeight: FontWeight.w300,
                                             ),
-                                            labelText: 'Cost',
+                                            labelText: 'Quantity',
                                           ),
                                         ),
                                       ),
+                                      const DropdownMenu(
+                                        enableFilter: true,
+                                        requestFocusOnTap: true,
+                                        leadingIcon: Icon(
+                                          Icons.search_outlined,
+                                          color: Colors.black,
+                                        ),
+                                        label: Text('Unit'),
+                                        inputDecorationTheme:
+                                            InputDecorationTheme(
+                                          border: UnderlineInputBorder(),
+                                        ),
+                                        dropdownMenuEntries: [
+                                          DropdownMenuEntry(
+                                              value: 'Ounce', label: 'Ounce'),
+                                          DropdownMenuEntry(
+                                              value: 'Gram', label: 'Gram'),
+                                          DropdownMenuEntry(
+                                              value: 'Pound', label: 'Pound'),
+                                          DropdownMenuEntry(
+                                              value: 'Gallon', label: 'Gallon'),
+                                        ],
+                                      )
                                     ],
                                   ),
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: TextFormField(
-                                        validator: (value) {
-                                          if (value == null || value.isEmpty) {
-                                            return 'Quantity required';
-                                          }
-                                          return null;
-                                        },
-                                        keyboardType: const TextInputType
-                                            .numberWithOptions(
-                                          decimal: true,
-                                          signed: false,
-                                        ),
-                                        decoration: const InputDecoration(
-                                          prefixIcon: Icon(
-                                            Icons.attach_money,
-                                            color: Colors.black,
-                                          ),
-                                          hintText: '',
-                                          hintStyle: TextStyle(
-                                            fontStyle: FontStyle.italic,
-                                            fontWeight: FontWeight.w300,
-                                          ),
-                                          labelText: 'Quantity',
-                                        ),
-                                      ),
+                                  const DropdownMenu(
+                                    enableFilter: true,
+                                    requestFocusOnTap: true,
+                                    leadingIcon: Icon(
+                                      Icons.search_outlined,
+                                      color: Colors.black,
                                     ),
-                                    const DropdownMenu(
-                                    
-                                      requestFocusOnTap: false,
-                                      label: Text('Unit'),
-                                      inputDecorationTheme:
-                                          InputDecorationTheme(
-                                        border: UnderlineInputBorder(),
-                                      ),
-                                      dropdownMenuEntries: [
-                                        DropdownMenuEntry(
-                                            value: 'Ounce', label: 'Ounce'),
-                                        DropdownMenuEntry(
-                                            value: 'Quart', label: 'Quart'),
-                                        DropdownMenuEntry(
-                                            value: 'Gram', label: 'Gram'),
-                                        DropdownMenuEntry(
-                                            value: 'Pound', label: 'Pound')
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ],
+                                    label: Text('Category'),
+                                    inputDecorationTheme: InputDecorationTheme(
+                                      border: UnderlineInputBorder(),
+                                    ),
+                                    dropdownMenuEntries: [
+                                      DropdownMenuEntry(
+                                          value: 'Ounce', label: 'Vegetables'),
+                                      DropdownMenuEntry(
+                                          value: 'Gram', label: 'Eggs'),
+                                      DropdownMenuEntry(
+                                          value: 'Pound', label: 'Pasta')
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                )
-              },
-              icon: const Icon(
-                Icons.add_circle,
-                color: Colors.black,
+                      );
+                    },
+                  )
+                },
               ),
             ),
           ],
